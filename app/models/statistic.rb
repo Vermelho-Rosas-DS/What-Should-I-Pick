316bb0b974b70c6cbcbc8be0455f900c63c61f40
elias <eliasoliveiradapaz@hotmail.com>
2022-12-04 15:32:44 -0300

    add model and enums

--- app/models/statistic.rb
+++ app/models/statistic.rb
@@ -1,5 +1,5 @@
 class Statistic < ApplicationRecord
     enum :tier, [:all,:iron,:bronze,:silver,:gold,:platinum,:diamond,:master,:gandmaster,:challenger]
-    enum :position, [:top,:jungle,:mid,:,:adc,:support]
+    enum :position, [:top,:jungle,:mid,:adc,:support]
     enum :region, [:global,:na,:euw,:eune,:oce,:kr,:jp,:br,:las,:lan,:ru,:tr]
 end
