23825b4c2d1ab11305cf459dbcf2d6dc0b082755
Elias Oliveira <eliasoliveiradapaz@hotmail.com>
2022-12-16 14:15:07 -0300

    link models

--- app/models/champion.rb
+++ app/models/champion.rb
@@ -1,7 +1,7 @@
 class Champion < ApplicationRecord
     has_many :statistics
     attr_accessor :pick_rate
+    
     def prencherpick()
-        Champion.statistics.pick_rate() = self.statistics.order(pick_rate: :desc).first
-        end
+        statistics.pick_rate = self.statistics.order(pick_rate: :desc).first
 end
