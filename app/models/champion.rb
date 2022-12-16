c125c4f14a509883d562b28ddeba089dd1a87189
Elias Oliveira <eliasoliveiradapaz@hotmail.com>
2022-12-16 15:00:44 -0300

    repair sintax model champion

--- app/models/champion.rb
+++ app/models/champion.rb
@@ -1,8 +1,8 @@
 class Champion < ApplicationRecord
     has_many :statistics
-    attr_accessor :pick_rate, tier:, :position, win_rate:, performance:, kill:, death:, assist:, champion_key:
+    attr_accessor :pick_rate, tier:, position:, win_rate:, performance:, kill:, death:, assist:, champion_key:
     
     def mostfrequentattribute()
-        statistics.pick_rate = self.statistics.order(pick_rate:, :tier, :position, :win_rate, :performance, :kill, :death, :assist, :champion_key :desc).first
+        statistics.pick_rate = self.statistics.order(:pick_rate, :tier, :position, :win_rate, :performance, :kill, :death, :assist, :champion_key :desc).first
     end
 end
