8ecb2eb325f207c6e50e78d1c9d3602291554608
Elias Oliveira <eliasoliveiradapaz@hotmail.com>
2022-12-16 15:24:32 -0300

    repair model

--- app/models/champion.rb
+++ app/models/champion.rb
@@ -1,8 +1,4 @@
 class Champion < ApplicationRecord
     has_many :statistics
     attr_accessor mostfrequentattribute:
-    
-    def mostfrequentattribute()
-        statistics.mostfrequentattribute = self.statistics.order(:pick_rate, :tier, :position, :win_rate, :performance, :kill, :death, :assist, :champion_key :desc)
-    end
 end
