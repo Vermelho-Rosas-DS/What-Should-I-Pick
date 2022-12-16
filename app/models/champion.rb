5a31f647efd19865e16028ea6eaae75a7cdca475
Elias Oliveira <eliasoliveiradapaz@hotmail.com>
2022-12-16 13:24:43 -0300

    link models

--- app/models/champion.rb
+++ app/models/champion.rb
@@ -1,2 +1,7 @@
 class Champion < ApplicationRecord
+    has_many :statistics
+    attr_accessor :pick_rate
+    def prencherpick()
+        Champion.statistics.pick_rate() = self.statistics.order(pick_rate: :desc).first
+        end
 end
--- app/models/statistic.rb
+++ app/models/statistic.rb
@@ -1,4 +1,5 @@
 class Statistic < ApplicationRecord
+  belongs_to :champion
   enum :tier, [:all, :iron, :bronze, :silver, :gold, :platinum, :diamond, :master, :grandmaster, :challenger]
   enum :position, [:top, :jungle, :mid, :adc, :support]
   enum :region, [:global, :na, :euw, :eune, :oce, :kr, :jp, :br, :las, :lan, :ru, :tr]
