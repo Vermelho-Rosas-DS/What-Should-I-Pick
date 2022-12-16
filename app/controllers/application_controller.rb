98bd5356b5a7e296b691bec3a03a4c63722efb48
GabrielCastelo-31 <gabflubr@outlook.com>
2022-12-16 16:47:25 -0300

    Traduções adicionadas. Erro na traduçaõ da barra de pesquisa

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
   def home
   end
+
+  around_action :switch_locale
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+  
+  def default_url_options
+    { locale: I18n.locale }
+  end
 end
