03763fe7a16613fc28677b3f43e3933b564457e3
Victor Gonçalves <victor.eng.unb@gmail.com>
2023-01-07 11:29:43 -0300

    fix: remove redundant root path from routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
-  root 'application#home'
 
   # Defines the locale scope
   scope '(:locale)', locale: /en|pt-BR|es/ do
-    get '/' => 'application#home'
+    root 'application#home'
     get 'inicio' => 'welcome#index'
   end
 end
