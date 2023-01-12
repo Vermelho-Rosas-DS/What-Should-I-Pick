fd1f4ceee23233e720c4f80fb8a255757844094b
elias <eliasoliveiradapaz@hotmail.com>
2023-01-12 17:54:29 -0300

    add model recommendation/enum

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -11,6 +11,6 @@ class RecommendationsController < ApplicationController
     estats = estats.champion_key
     @champ = Champion.where(key: estats).first
     redirect_to recommendations_path(id: @champ.id)
-    @recommendation = Recommendation.create(champion_key: params[:champion_key],win_rate: params[:win_rate],pick_rate: params[:pick_rate])
+    @recommendation = Recommendation.create(champion_key: params[:champion_key], win_rate: params[:win_rate], pick_rate: params[:pick_rate])
   end
 end
