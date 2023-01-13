65deb38d223849be47f39cc3570bd1cc73d740b8
GabrielCastelo-31 <gabflubr@outlook.com>
2023-01-13 20:43:03 -0300

    Recommendation action added. Champion_key error fixed.

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -8,9 +8,17 @@ class RecommendationsController < ApplicationController
 
   def create
     estats = Statistic.where(tier: params[:tier], position: params[:position], role: params[:role]).order(:win_rate).last
-    estats = estats.champion_key
-    @champ = Champion.where(key: estats).first
-    redirect_to recommendations_path(id: @champ.id)
-    @recommendation = Recommendation.create(champion_key: params[:champion_key], win_rate: params[:win_rate], pick_rate: params[:pick_rate])
+    if estats.present?
+      champ_key = estats.champion_key
+      @champ = Champion.where(key: champ_key).first
+      @recommendation = Recommendation.create(champion_key: estats.champion_key, win_rate: estats.win_rate, pick_rate: estats.pick_rate)
+      if @recommendation.persisted?
+        redirect_to recommendations_path(id: @champ.id)
+      else
+        redirect_to root_path
+      end
+    else
+      redirect_to root_path
+    end
   end
 end
