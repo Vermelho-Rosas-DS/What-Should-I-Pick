ed57b39e191c4048f893ab2ce1a2ee77d9f781e3
Victor Gonçalves <victor.eng.unb@gmail.com>
2023-01-23 23:01:25 -0300

    remove wrong code

--- test/models/champion_test.rb
+++ test/models/champion_test.rb
@@ -1,9 +1,4 @@
 require 'test_helper'
 
 class ChampionTest < ActiveSupport::TestCase
-  test 'Should return 2 on function return_2' do
-    champion = Champion.new
-
-    assert_equal 5, champion.retorna_2
-  end
 end
