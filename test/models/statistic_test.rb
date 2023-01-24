3b37da7672e91470e6fc2eaa5f1a9e080f41ed6f
Victor Gonçalves <victor.eng.unb@gmail.com>
2023-01-24 00:57:59 -0300

    test: create tests for filtered by method

--- test/models/statistic_test.rb
+++ test/models/statistic_test.rb
@@ -1,5 +1,26 @@
+require 'test_helper'
+
 class StatisticTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+  # test 'Should filter statistics correctly when using filtered_by method' do
+  #   statistics = Statistic.filtered_by(tier: 'gold', position: 'top')
+  #   statistics.each do |s|
+  #     assert_equal 'gold', s.tier.to_s
+  #     assert_equal 'top', s.position.to_s
+  #   end
+
+  #   statistics = Statistic.filtered_by(tier: 'gold', position: 'mid', role: 'fighter')
+  #   statistics.each do |s|
+  #     assert_equal 'gold', s.tier.to_s
+  #     assert_equal 'mid', s.position.to_s
+  #   end
+
+  #   statistics = Statistic.filtered_by(tier: 'diamond', position: 'mid', role: 'fighter')
+  #   statistics.each do |s|
+  #     assert_equal 'diamond', s.tier.to_s
+  #     assert_equal 'mid', s.position.to_s
+  #   end
+
+  #   expected_result = [statistics(:aatrox_mid_gold), statistics(:garen_mid_gold)].sort
+  #   assert_equal expected_result, Statistic.filtered_by(tier: 'gold').to_a.sort
   # end
 end
