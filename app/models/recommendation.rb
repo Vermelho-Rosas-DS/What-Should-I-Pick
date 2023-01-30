a5954155be73540e22babce528060e6e8443c3c2
Victor Gonçalves <victor.eng.unb@gmail.com>
2023-01-30 13:37:16 -0300

    add inverse of

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -1,4 +1,4 @@
 class Recommendation < ApplicationRecord
-  belongs_to :champion, foreign_key: :champion_key, primary_key: :key, class_name: "Champion"
+  belongs_to :champion, inverse_of: :recommendations, foreign_key: :champion_key, primary_key: :key, class_name: 'Champion'
   enum feedback_score: { is_negative: 0, is_positive: 1 }, _prefix: true
 end
